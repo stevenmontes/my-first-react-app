d9561b3c53669fd9c5efe2374eaf12da269b765c
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-18 20:02:05 -0600

    44. Evento click (Eventos en general)

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 
+const handleAdd = () => {
+    console.log('+1');
+}
+
 export const CounterApp = ({ value }) => {
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+
+            <button onClick={handleAdd}>
+                +1
+            </button>
         </>
     )
 }
