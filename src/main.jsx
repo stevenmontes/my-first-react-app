1114d561ff2520c6ec801ad6bfae64821c7f9a13
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-18 19:32:21 -0600

    43. Tarea - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './FirstApp'
+import CounterApp from './CounterApp'
 
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <CounterApp value={0} />
     </React.StrictMode>
 );
