2613346a5e8df586ff7562059c3523b2523f69a8
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-18 19:02:20 -0600

    41. PropTypes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,18 @@
-const getMessage = (name) => `Hola ${name}`;
+import PropTypes from 'prop-types';
 
 const FirstApp = ({ title, subTitle }) => {
+
     return (
         <>
             <h1>{title}</h1>
-            <p>{subTitle + 1}</p>
+            <p>{subTitle}</p>
         </>
     )
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired
 }
 
 export default FirstApp;
