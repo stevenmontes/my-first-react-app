431e86cbca946b95f66d6de7b74c4b9856bd8733
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-20 19:41:20 -0600

    53. Jest - Expect - toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,12 @@
-test('Prueba no debe fallar', () => {
-    if (1 === 0) {
-        throw new Error('No puede dividir entre 0')
-    }
+describe('Pruebas en <DemoComponet>', () => {
+    test('Prueba no debe fallar', () => {
+        // 1. Arrange
+        const message = "Hola mundo";
+
+        // 2. Act
+        const message2 = message.trim();
+
+        // 3.Assert
+        expect(message).toBe(message2);
+    })
 })
