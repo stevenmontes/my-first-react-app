3b375956d543bdb285c1248ee25d19c5ce1577cc
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-27 19:41:37 -0600

    64. Probar FirstApp

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -3,6 +3,19 @@ import FirstApp from "../src/FirstApp";
 
 describe('Pruebas en FirstApp', () => {
     test('should match with the snapshot', () => {
-        render(<FirstApp title="Hola, Soy Goku" />);
+        const { container } = render(<FirstApp title="Hola, Soy Goku" />);
+
+        expect(container).toMatchSnapshot();
     });
+
+    test('should show a title in h1', () => {
+        const title = "Hola, Soy Goku";
+        const { container, getByText } = render(<FirstApp title={title} />);
+
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
+
+    })
 });
