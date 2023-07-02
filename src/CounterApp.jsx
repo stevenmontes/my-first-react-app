865141f52bececc6cb5eda842774c5f27973cb94
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-07-01 18:40:35 -0600

    68. Simular eventos - Click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -24,7 +24,7 @@ export const CounterApp = ({ value }) => {
 
             <button onClick={handleAdd}>+1</button>
             <button onClick={handleSubtract}>-1</button>
-            <button onClick={handleReset}>Reset</button>
+            <button aria-label='btn-reset' onClick={handleReset}>Reset</button>
         </>
     )
 }
--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import CounterApp from '../src/CounterApp';
 
 describe('tests of CounterAppp', () => {
@@ -12,5 +12,28 @@ describe('tests of CounterAppp', () => {
     test('should show the initial value of 100', () => {
         render(<CounterApp value={100} />);
         expect(screen.getByText('100')).toBeTruthy();
-    })
+    });
+
+    test('should increase with the button +1', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('1')).toBeTruthy();
+    });
+
+    test('should decrease with the button -1', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByRole('heading', { level: 2 }).innerHTML).toBe('-1');
+    });
+
+    test('should work the button "Reset"', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        /* fireEvent.click(screen.getByText('Reset')); */
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+
+        expect(screen.getByText(initialValue)).toBeTruthy();
+    });
 })
